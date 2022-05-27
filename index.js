f9938389386af33148c57c0192f47c0c1989a1be
Justin James <justinmjames@protonmail.com>
2022-05-26 19:24:49 -0700

    commit tree a85f1a381f4e65170f2aa86b1c3bd2a745b7fc2a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+    let wins = record.find(object => object.result === 'W')
+     if (wins){
+       return wins.year
+    }
+ }
+ 
+superbowlWin(record);
+
+
